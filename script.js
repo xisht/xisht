747df92a7e01963f7c29b614c1cf8cade1653774
xisht <53704975+xisht@users.noreply.github.com>
2019-08-04 23:40:24 +0300

    Add contact info and change card shape

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var canv = document.getElementById("canvas");
 ctx = canv.getContext("2d");
 
-var shapeHeight = 30;
-var shapeWidth = 30;
-var colGap = Math.floor(shapeWidth/2);
-var rowGap = Math.floor(shapeHeight/2);
+var shapeHeight = 20;
+var shapeWidth = 20;
+var colGap = Math.floor(shapeWidth);
+var rowGap = Math.floor(shapeHeight);
 var rectHeight = Math.floor(shapeHeight / 3);
 var colors = ["#ffc234", "#ee384e", "#28406e", "#4fc5df"];
 
@@ -20,7 +20,7 @@ function init() {
   for (var iCol = 0; iCol < cols; iCol++) {
     for (var iRow = 0; iRow < rows; iRow++) {
 
-      var shape = rand(0, 2);
+      var shape = rand(0, 3);
       var colOffset = iCol * (shapeWidth + colGap);
       var rowOffset = iRow * (shapeHeight + rowGap);
 
@@ -29,7 +29,8 @@ function init() {
       switch(shape) {
         case 0: drawTraingle(colOffset, rowOffset); break;
         case 1: drawCircle(colOffset, rowOffset); break;
-        case 2: drawRect(colOffset, rowOffset); break;
+        case 2: drawSquare(colOffset, rowOffset); break;
+        case 3: drawRect(colOffset, rowOffset); break;
       }
 
       ctx.fillStyle = colors[rand(0, 3)];
@@ -54,10 +55,27 @@ function drawRect(colOffset, rowOffset) {
   ctx.rect(colOffset, rowOffset + shapeHeight/2 - rectHeight/2, shapeWidth, rectHeight);
 }
 
+function drawSquare(colOffset, rowOffset) {
+  ctx.rect(colOffset, rowOffset, shapeWidth, shapeHeight);
+}
+
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+window.pause = false;
+
 window.addEventListener("click", init);
 window.addEventListener("resize", init);
+
+// window.addEventListener("mousemove", function () {
+//   if (!window.pause) {
+//     window.pause = true;
+//     init();
+//     setTimeout(function () {
+//       window.pause = false;
+//     }, 100);
+//   }
+// });
+
 init();
